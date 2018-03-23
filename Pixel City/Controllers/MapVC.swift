bb228c2ef7c0653d9a661e9385a0db160799bfc4
Andrii Halabuda <losAngelesBoy11@gmail.com>
2018-03-23 17:07:44 +0100

    Added bottom view animation up B6

--- Pixel City/Controllers/MapVC.swift
+++ Pixel City/Controllers/MapVC.swift
@@ -13,6 +13,9 @@ import CoreLocation
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
+    
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
@@ -33,6 +36,35 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         map.addGestureRecognizer(doubleTap)
     }
     
+    func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    func animateViewUp() {
+        pullUpViewHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func animateViewDown() {
+        pullUpViewHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+//    func addSpinner() {
+//        spinner = UIActivityIndicatorView()
+//        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+//        spinner?.activityIndicatorViewStyle = .whiteLarge
+//        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+//        spinner?.startAnimating()
+//        pullUpView.addSubview(spinner!)
+//    }
+    
     @IBAction func centerBtnTapped(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
             centerMapOnUserLocation()
@@ -42,6 +74,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
 }
 
 extension MapVC: MKMapViewDelegate {
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             return nil
@@ -61,6 +94,7 @@ extension MapVC: MKMapViewDelegate {
     
     @objc func dropPin(sender: UITapGestureRecognizer) {
         removePin()
+        animateViewUp()
         
         let touchPoint = sender.location(in: map)
         let touchCoordinate = map.convert(touchPoint, toCoordinateFrom: map)
