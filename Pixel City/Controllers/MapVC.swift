4e38aa2d32bb0ce209e1530b0a13e87a3733ed32
Andrii Halabuda <losAngelesBoy11@gmail.com>
2018-03-23 16:44:33 +0100

    Pin drop setup B5

--- Pixel City/Controllers/MapVC.swift
+++ Pixel City/Controllers/MapVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     var locationManager = CLLocationManager()
@@ -23,8 +23,15 @@ class MapVC: UIViewController {
         map.delegate = self
         locationManager.delegate = self
         configureLocationServices()
+        addDoubleTap()
     }
 
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        map.addGestureRecognizer(doubleTap)
+    }
     
     @IBAction func centerBtnTapped(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
@@ -35,17 +42,44 @@ class MapVC: UIViewController {
 }
 
 extension MapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = UIColor.red
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+    }
     
     func centerMapOnUserLocation() {
-        
         guard let coordinate = locationManager.location?.coordinate else { return }
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
         map.setRegion(coordinateRegion, animated: true)
     }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer) {
+        removePin()
+        
+        let touchPoint = sender.location(in: map)
+        let touchCoordinate = map.convert(touchPoint, toCoordinateFrom: map)
+        
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        map.addAnnotation(annotation)
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func removePin() {
+        for annotation in map.annotations {
+            map.removeAnnotation(annotation)
+        }
+    }
 }
 
 extension MapVC: CLLocationManagerDelegate {
-    
     func configureLocationServices() {
         if authorizationStatus == .notDetermined {
             locationManager.requestAlwaysAuthorization()
